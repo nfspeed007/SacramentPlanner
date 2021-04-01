2825c058f8ad4204d693d94558c1a6157003583d
nfspeed007 <isr15001@byui.edu>
2021-03-31 19:18:37 -0600

    members

---
+++ SacramentPlanner/Models/Members.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentPlanner.Models
+{
+    public enum Calling
+    {
+        Bishop,
+        Councilor_In_Bishopric,
+        Relief_Society_President,
+        Elders_Quorum_President,
+        Primary_President,
+        Young_Mens_President,
+        Young_Womens_President,
+        Other,
+        No_Calling
+    }
+
+    public class Members
+    {
+
+        public int MembersID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Calling? Calling { get; set; }
+    }
+}
---
+++ SacramentPlanner/Models/SacramentPlanner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentPlanner.Models
+{
+    public class SacramentPlanner
+    {
+        public int ID { get; set; }
+        public string Conducting { get; set; }
+        public string OpeningHymn { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
